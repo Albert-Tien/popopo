a02d082f0afc9abe64dd85e211e0352994cfc43a
Albert-Tien <1813500720@qq.com>
2018-06-06 15:36:09 +0800

    第二次修改，这修改增加了5g所选项。

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -27,3 +27,13 @@ class TestNetwork:
         self.network_page.click_network()
         self.network_page.click_first_network()
         self.network_page.click_3g_network()
+    def test_mobile_network_4g(self):
+        self.network_page.click_more()
+        self.network_page.click_network()
+        self.network_page.click_first_network()
+        self.network_page.click_3g_network()
+    def test_mobile_network_5g(self):
+        self.network_page.click_more()
+        self.network_page.click_network()
+        self.network_page.click_first_network()
+        self.network_page.click_3g_network()
